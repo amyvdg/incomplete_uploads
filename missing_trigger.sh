c0e661cadb51789c761cbfae05418e2c387a7fe7
amyvdg <amyvandergon@gmail.com>
2017-02-21 15:23:52 -0800

    Update missing_trigger.sh
    Now adds trigger files to the subfolders within batches.

--- missing_trigger.sh
+++ missing_trigger.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
-umgi_base=/ng_edrop_a/umgi/incoming
+base=/ng_edrop_a/umgi/incoming
 cd /ng_edrop_a/umgi/incoming
 umask 0002
 function find_problems
 {
-	find -mindepth 1 -maxdepth 1 -mtime +2 -type d | while read -r D; do test -z "$(shopt -s nullglob; printf "%s\n" "$D"/*.complete)" && printf "%s\n" "$D"; done > "$umgi_base/UMGI-EMI_missing_trigger.txt"
+	find -mindepth 1 -maxdepth 1 -type d | while read -r D; do test -z "$(shopt -s nullglob; printf "%s\n" "$D"/*.complete)" && printf "%s\n" "$D"; done > "$base/UMGI-EMI_missing_trigger.txt"
 }
 
+function find_batches
+{
+	while IFS= read -r line ;
+	do
+		printf '%s\n' "$line" > temp.txt
+		find -mindepth 2 -maxdepth 2 -type d | while read -r D; do test -z "$(shopt -s nullglob; printf "%s\n" "$D"/*.complete)" && printf "%s\n" "$D"; done > "$base/missing_batch.txt"
+	done < "$base/UMGI-EMI_missing_trigger.txt"
+}
+
+function add_files
+{
+	while IFS= read -r batches ;
+	do
+		printf '%s\n' "$batches" >> temp.txt
+		echo > $batches/delivery.complete
+	done < "$base/missing_batch.txt"
+}
+
+function make_log
+{
+	echo "These folders were incorrectly set up as batches:" > $base/log.txt
+	cat $base/UMGI-EMI_missing_trigger.txt >> $base/log.txt
+	echo -e "\nTrigger files were automatically added to these folders:" >> $base/log.txt
+	cat $base/missing_batch.txt >> $base/log.txt
+}
+	
 function alert_problems
 {
-	if [ -s $umgi_base/UMGI-EMI_missing_trigger.txt ]
+	if [ -s $base/UMGI-EMI_missing_trigger.txt ]
 	then
-		echo -e "EMI forgot to add delivery.complete files again. Go to the ng_edrop_a/umgi folder and add them to the folders below. \n\n(This is an automated message - $umgi_base/mising_trigger.sh running on edr02.)\n\n" | cat - $umgi_base/UMGI-EMI_missing_trigger.txt | mail -r cops@mndigital.com -s "EMI ZA trigger files missing" contentops@mndigital.com
+		find_batches ;
+		echo -e "EMI forgot to add delivery.complete files again. (This is an automated message.)\n" | cat - $base/log.txt | mail -r cops@mndigital.com -s "EMI ZA trigger files missing" ContentOps@mndigital.com
 	fi
 }
 
 find_problems
+add_files
+make_log
 alert_problems
